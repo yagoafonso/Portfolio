ad51404d6932f49a9b567505758fb98b1affcf0d
Yago Afonso <yago.afonso@hotmail.com>
2022-09-20 19:51:41 -0400

    Add project Nlw Esports

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { ProjectCard } from "../../components/ProjectCard/ProjectCard";
-import  ImgProjectFeedback  from "../../assets/projeto_nlw_return.jpg";
-import ImgProjectIgniteFeed from "../../assets/ignitefeed.jpg";
-import ImgProjectCloneTwitter from "../../assets/clone_twitter.jpg"
+import  imgProjectFeedback  from "../../assets/projeto_nlw_return.jpg";
+import imgProjectIgniteFeed from "../../assets/ignitefeed.jpg";
+import imgProjectEsports from "../../assets/projeto_nlw_esports.jpg";
 
 import { 
   ContainerProjects,
@@ -18,18 +18,24 @@ export function Projects(){
       <ContainerText>Aqui estão alguns projetos que trabalhei recentemente:</ContainerText>
       <ContainerProjectCards>
         <ProjectCard
-          src={ImgProjectFeedback}
+          src={imgProjectFeedback}
           title="Feedback"
-          text="Durante a 8º edição do evento na trilha Impulse realizado pela Rocketseat, foi desenvolvida um widget completo e acessível que pode ser utilizado em múltiplos projetos para coletar feedbacks dos usuários."
+          text="Durante a 8º edição do evento NLW na trilha Impulse realizado pela Rocketseat, foi desenvolvida um widget completo e acessível que pode ser utilizado em múltiplos projetos para coletar feedbacks dos usuários."
           linkGitHub="https://github.com/yagoafonso/nlw_return_impulse"
           linkDemo="https://nlw-return-impulse-pink-two.vercel.app/"
         />
         <ProjectCard
-          src={ImgProjectIgniteFeed}
+          src={imgProjectIgniteFeed}
           title="Ignite Feed"
           text="Projeto desenvolvido durante o primeiro módulo do Ignite - Trilha ReactJs oferecido pela Rocketset."
           linkGitHub="https://github.com/yagoafonso/ignite-feed"
           linkDemo="https://ignitefeed-xi.vercel.app/"
+        />
+        <ProjectCard 
+          src={imgProjectEsports}
+          text="Na 9ª edição do evento NLW na trilha Ignite realizado pela Rocketseat, o projeto foi voltado para o universo dos games. Na aplicação você consegue se cadastrar e ver as pessoas que estão procurando por um duo no game."
+          linkGitHub="https://github.com/yagoafonso/nlw_esports"
+          linkDemo="https://nlw-esports-nu.vercel.app/"
         />
       </ContainerProjectCards>
     </ContainerProjects>

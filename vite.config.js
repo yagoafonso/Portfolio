97e2989329da4acb0ce4ad6a5462fe4edca7ca46
Yago Afonso <yago.afonso@hotmail.com>
2022-09-24 15:31:04 -0400

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://yagoafonso.github.io/yagoafonso.github.io',
+  base: 'https://yagoafonso.github.io/',
   plugins: [react()]
 })
